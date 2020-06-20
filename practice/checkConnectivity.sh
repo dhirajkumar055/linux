a59a016fa6595346adf1fcfe9ad7457fad9241b0
Dheeraj Kumar <dheeraj.chopra@paytm.com>
2020-06-20 23:25:33 +0530

    First Commit

---
+++ practice/checkConnectivity.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#check=`ping -c 1 -q google.com >&/dev/null; echo $?`
+#echo ${check}
+
+
+wget -q --spider https://google.com
+
+if [ $? -eq 0 ]; then
+    echo "Online"
+else
+    echo "Offline"
+fi
---
+++ practice/set.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#set -euo pipefail
+#a=DSF
+#echo $a
+echo $1
+set `date`
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+set +u
+
+#echo $1
+#Set the arguments
